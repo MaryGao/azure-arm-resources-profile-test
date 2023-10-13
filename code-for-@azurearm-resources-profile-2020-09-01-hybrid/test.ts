b9482e54f53201428cb489908886b66ebe11acfd
Mary Gao <yanmeigao1210@gmail.com>
2023-10-13 16:34:32 +0800

    Update the test cases

--- code-for-@azurearm-resources-profile-2020-09-01-hybrid/test.ts
+++ code-for-@azurearm-resources-profile-2020-09-01-hybrid/test.ts
@@ -1,13 +1,18 @@
-import { DefaultAzureCredential } from "@azure/identity";
+import { AzureAuthorityHosts, DefaultAzureCredential } from "@azure/identity";
 import { ResourceManagementClient } from "@azure/arm-resources-profile-2020-09-01-hybrid";
 
 const subscriptionId = process.env.SUBSCRIPTION_ID || '';
 // Read the link for more information about ClientSecretCredential - https://github.com/Azure/azure-sdk-for-js/blob/main/documentation/MIGRATION-guide-for-next-generation-management-libraries.md#authentication
-const credential = new DefaultAzureCredential();
+
 // const credentials = new ClientSecretCredential(tenantId, clientId, clientSecrat);
 const resourceGroup = "myjstest12";
 const location = "eastus";
-const client = new ResourceManagementClient(credential, subscriptionId);
+
+const endpoint = "https://management.usgovcloudapi.net";
+const credential = new DefaultAzureCredential({authorityHost: AzureAuthorityHosts.AzureGovernment});
+const client = new ResourceManagementClient(credential, subscriptionId, {
+  credentialScopes: [`"${endpoint}/.default`],
+});
 
 async function test() {
   const result = await client.resourceGroups.createOrUpdate(
